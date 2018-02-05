f20127d18066d54290f72da34f0ba215bf124154
davidfordaus <davidfordaus@gmail.com>
2018-02-05 17:10:45 +1100

    initial

---
+++ 1/src/main/java/test/App.java
@@ -0,0 +1,16 @@
+package test;
+
+import test.model.Customer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Customer f=new Customer();
+        System.out.println( "Hello World!" );
+    }
+}
